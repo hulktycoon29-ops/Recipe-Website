0c8dabb2e18db0496c7681ec1a28aa8363a65bcc
Priyanshu Gupta <priyanshupg29@gmail.com>
2026-05-11 10:55:38 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function showRecipe(id) {
+            let recipes = document.querySelectorAll('.recipe');
+
+            recipes.forEach(r => r.style.display = "none");
+
+            let selected = document.getElementById(id);
+            selected.style.display = "block";
+        }
+
+        function closeRecipe(id) {
+            document.getElementById(id).style.display = "none";
+        }
